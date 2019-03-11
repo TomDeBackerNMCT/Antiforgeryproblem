3ecadb080080a4834ac3137871426746a562ea72
Tom NMCT <tom.de.backer3@student.howest.be>
2019-03-11 11:47:10 +0100

    First commit

---
+++ MVC_Hermaken/Data/MovieContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Hermaken.Models
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext (DbContextOptions<MovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_Hermaken.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ MVC_Hermaken/Models/Movie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVC_Hermaken.Models
+{
+    public class Movie
+    {
+
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required(ErrorMessage = "Please, enter a valid title in text form.")]
+        [DataType(DataType.Text)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please, enter a valid date.")]
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$")]
+        [StringLength(30)]
+        [Required(ErrorMessage = "Please, enter a valid genre in text form.")]
+        [DataType(DataType.Text)]
+        public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [Required(ErrorMessage = "Please, enter a valid price. (Must be a number)")]
+        [Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        [StringLength(1)]
+        [Required]
+        public string Rating { get; set; }
+    }
+}
